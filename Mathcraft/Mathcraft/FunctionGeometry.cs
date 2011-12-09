6911b130c6950c6bec310174eb38e425869842c5
Mathias Kahl <mathias.kahl@googlemail.com>
2011-12-09 15:04:22 +0100

    fuer IronPython entschieden. Parametrisierung v. Geoemtrie

--- Mathcraft/Mathcraft/FunctionGeometry.cs
+++ Mathcraft/Mathcraft/FunctionGeometry.cs
@@ -2,9 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Mathcraft
-{
+{    
+    class GeometryParameters
+    {
+        public Point3D Position;
+
+        // determines whether a point should be visible
+        public Func<bool, Point3D> Visible;
+
+        // determines the color of the point
+        public Func<Color, Point3D> Color;
+    }
+
     /// <summary>
     /// Geometry defined by a mathematical function or equations
     /// </summary>
---
+++ Mathcraft/Mathcraft/Point3D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mathcraft
+{
+    class Point3D
+    {
+        public int X, Y, Z;
+    }
+}
